2cfadfba0d4d9f4f4c1e7cf5102d8eb660bbf564
Selin <selin2002bayrak@gmail.com>
2025-12-15 22:25:28 +0300

    main file written

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+import { Student } from "./models.js";
+import { fetchStudents } from "./database.js";
+import {
+  calculateClassAverage,
+  findTopStudent,
+  filterStudents
+} from "./analytics.js";
+
+fetchStudents(rawData => {
+  console.log("Data received!\n");
+
+  const students = rawData.map(
+    s => new Student(s.id, s.name, s.courses)
+  );
+
+  console.log("Testing Immutability:");
+  console.log("Original ID:", students[0].id);
+  console.log("Attempting to change ID to 999...");
+  students[0].id = 999;
+  console.log("Final ID:", students[0].id, "(Success: ID did not change)\n");
+
+  console.log("--- Analytics Report ---");
+
+  const avg101 = calculateClassAverage(students, 101);
+  console.log("Class Average for Course 101:", avg101);
+
+  const topStudent = findTopStudent(students);
+  console.log(
+    `Top Student: ${topStudent.name} (Average: ${topStudent.getAverage().toFixed(2)})`
+  );
+
+  const course102Students = filterStudents(
+    students,
+    s => s.courses.some(c => c.courseId === 102)
+  );
+
+  console.log(
+    "Students in Course 102:",
+    course102Students.map(s => s.name).join(", ")
+  );
+});
